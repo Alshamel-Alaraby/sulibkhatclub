494cb3714b79a64b913637dce706590d2c5b9215
Ali Khalifa <ali689.kh@gmail.com>
2023-08-29 19:41:30 +0300

    last

--- Modules/ClubMembers/Http/Requests/CmMemberRequest.php
+++ Modules/ClubMembers/Http/Requests/CmMemberRequest.php
@@ -53,7 +53,7 @@ class CmMemberRequest extends FormRequest
             'member_type_id' => 'nullable|string|max:255',
             'notes' => 'nullable|string|max:255',
             'gender' => 'nullable|in:0,1',
-            'financial_year_id' => 'required|exists:general_financial_years,id',
+            'financial_year_id' => 'null|exists:general_financial_years,id',
             "company_id"=>'nullable',
         ];
     }
