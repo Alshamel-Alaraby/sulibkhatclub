29133321a66b22835b78853154656d9203cc1790
IslamKhaled18 <islam.hegazy72@gmail.com>
2023-09-12 14:29:13 +0300

    last

--- Modules/ClubMembers/Http/Controllers/CmStaticsController.php
+++ Modules/ClubMembers/Http/Controllers/CmStaticsController.php
@@ -20,35 +20,32 @@ class CmStaticsController extends Controller
         // // الأعضاء المشطوبين
         $data['DeletedMember'] = CmMember::where('member_status_id', 2)->count();
 
-          // // الأعضاء المشطوبين شطب بناءا علي طلبه
-        $data['count_6'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id',6)->count();
-        $data['Percentage_6'] = $data['DeletedMember'] != 0 ? round(($data['count_6'] / $data['DeletedMember'] ) * 100,2):0;
+        // // الأعضاء المشطوبين شطب بناءا علي طلبه
+        $data['count_6'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id', 6)->count();
+        $data['Percentage_6'] = $data['DeletedMember'] != 0 ? round(($data['count_6'] / $data['DeletedMember']) * 100, 2) : 0;
 
         // // الأعضاء المشطوبين شطب للوفاة
-        $data['count_7'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id',7)->count();
-        $data['Percentage_7'] = $data['DeletedMember'] != 0 ? round(($data['count_7'] / $data['DeletedMember'] ) * 100,2):0;
+        $data['count_7'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id', 7)->count();
+        $data['Percentage_7'] = $data['DeletedMember'] != 0 ? round(($data['count_7'] / $data['DeletedMember']) * 100, 2) : 0;
 
         // // الأعضاء المشطوبين شطب بموجب القرار الوزاري
-        $data['count_8'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id',8)->count();
-        $data['Percentage_8'] = $data['DeletedMember'] != 0 ? round(($data['count_8'] / $data['DeletedMember'] ) * 100,2):0;
+        $data['count_8'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id', 8)->count();
+        $data['Percentage_8'] = $data['DeletedMember'] != 0 ? round(($data['count_8'] / $data['DeletedMember']) * 100, 2) : 0;
 
         // // الأعضاء المشطوبين شطب بموجب كتاب الهيئة
-        $data['count_9'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id',9)->count();
-        $data['Percentage_9'] = $data['DeletedMember'] != 0 ? round(($data['count_9'] / $data['DeletedMember'] ) * 100,2):0;
+        $data['count_9'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id', 9)->count();
+        $data['Percentage_9'] = $data['DeletedMember'] != 0 ? round(($data['count_9'] / $data['DeletedMember']) * 100, 2) : 0;
 
         // // الأعضاء المشطوبين شطب لعدم السداد
-        $data['count_18'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id',18)->count();
-        $data['Percentage_18'] = $data['DeletedMember'] != 0 ? round(($data['count_18'] / $data['DeletedMember'] ) * 100,2):0;
-
+        $data['count_18'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id', 18)->count();
+        $data['Percentage_18'] = $data['DeletedMember'] != 0 ? round(($data['count_18'] / $data['DeletedMember']) * 100, 2) : 0;
 
         $CmMemberTypes = CmMemberType::all();
-        foreach ($CmMemberTypes as  $index => $CmMemberType ){
-            $data['CountDeletedCmMemberType'][$index][$CmMemberType->name]      = CmMember::where('member_status_id', 2)->where('member_kind_id',$CmMemberType->id)->count();
+        foreach ($CmMemberTypes as $index => $CmMemberType) {
+            $data['CountDeletedCmMemberType'][$index][$CmMemberType->name] = CmMember::where('member_status_id', 2)->where('member_kind_id', $CmMemberType->id)->count();
         }
         return $data;
     }
-
-
 
     public function getStatics()
     {
@@ -95,7 +92,6 @@ class CmStaticsController extends Controller
         $data['PaidPersonHasNoRightToAttendPercentage'] = $data['normalMemberCount'] != 0 ? round(($data['PaidPersonHasNoRightToAttend'] / $data['normalMemberCount']) * 100, 2) : 0;
 
         // //////////////////////////////////////////    المشطوبين    //////////////////////////////////////////////////////////
-
 
         // // شطب بناءا علي طلبه
         $data['DeletedAtHisRequest'] = CmMember::where('member_type_id', 6)->count();
@@ -145,7 +141,6 @@ class CmStaticsController extends Controller
 
         return response()->json($response);
     }
-
 
     public function validMembers()
     {
@@ -224,7 +219,6 @@ class CmStaticsController extends Controller
         $data['count_18'] = CmMember::where('member_status_id', 2)->where('discharge_reson_id', 18)->count();
         $data['Percentage_18'] = $data['DeletedMember'] != 0 ? round(($data['count_18'] / $data['DeletedMember']) * 100, 2) : 0;
 
-
         $CmMemberTypes = CmMemberType::all();
         foreach ($CmMemberTypes as $index => $CmMemberType) {
             $memberTypeCount = CmMember::where('member_status_id', 2)->where('member_kind_id', $CmMemberType->id)->count();
@@ -240,10 +234,8 @@ class CmStaticsController extends Controller
             ];
         }
 
-
-
 //المجموعات
-$data['groups'] = CmSponsorGroup::count();
+        $data['groups'] = CmSponsorGroup::count();
 //الرعاه
         $data['sponsors'] = CmSponser::count();
 /////////////// count of sponsor for each group /////////////////////
@@ -263,13 +255,13 @@ $data['groups'] = CmSponsorGroup::count();
             ];
         });
 
+        /////////////// count of members for each sponsor where (member_status_id) == 1 /////////////////////
+
         $memberCounts = CmSponser::select('id', 'name', 'name_e')
             ->withCount(['members' => function ($query) {
                 $query->where('member_status_id', 1);
             }])
             ->get();
-
-        /////////////// count of members for each sponsor where (member_status_id) == 1 /////////////////////
 
         $totalMembers = $memberCounts->sum('members_count');
 
@@ -289,52 +281,55 @@ $data['groups'] = CmSponsorGroup::count();
         })->values();
 ////////// count of members for each sponsor where (member_status_id) == 1  and for every permission ///////////
 
-$totalMembersPermissions = CmMember::where('member_status_id', 1)->count();
+        $totalMembersPermissions = CmMember::where('member_status_id', 1)->count();
 
-$memberCountsPermissions = CmSponser::select('id', 'name', 'name_e')
-    ->withCount(['members' => function ($query) {
-        $query->where('member_status_id', 1);
-    }])
-    ->get();
+        $memberCountsPermissions = CmSponser::select('id', 'name', 'name_e')
+            ->withCount(['members' => function ($query) {
+                $query->where('member_status_id', 1);
+            }])
+            ->get();
 
-$data['sponsors_members_permissions'] = $memberCountsPermissions->map(function ($sponsor) use ($totalMembersPermissions) {
-    $permissions = CmMemberPermission::all();
+            $data['sponsors_members_permissions'] = $memberCountsPermissions->map(function ($sponsor) use ($totalMembersPermissions) {
+                $permissions = CmMemberPermission::all();
 
-    $counts = [];
+                $counts = [];
 
-    foreach ($permissions as $permission) {
-        $count = $sponsor->members()
-            ->where('member_status_id', 1)
-            ->where('members_permissions_id', $permission->id)
-            ->count();
+                foreach ($permissions as $permission) {
+                    $count = $sponsor->members()
+                        ->where('member_status_id', 1)
+                        ->where('members_permissions_id', $permission->id)
+                        ->count();
 
-        $percentage = ($count / $totalMembersPermissions) * 100;
+                    // Only add entries with a non-zero count.
+                    if ($count > 0) {
+                        $percentage = ($count / $totalMembersPermissions) * 100;
 
-        if ($count > 0) {
-            $counts[] = [
-                'id' => $permission->id,
-                'name' => $permission->name,
-                'name_e' => $permission->name_e,
-                'count' => $count,
-                'percentage' => round($percentage, 2),
-            ];
-        }
-    }
+                        $counts[] = [
+                            'id' => $permission->id,
+                            'name' => $permission->name,
+                            'name_e' => $permission->name_e,
+                            'count' => $count,
+                            'percentage' => round($percentage, 2),
+                        ];
+                    }
+                }
 
-    if ($sponsor->members_count > 0) {
-        return [
-            'id' => $sponsor->id,
-            'name' => $sponsor->name,
-            'name_e' => $sponsor->name_e,
-            'count' => $sponsor->members_count,
-            'percentage' => round(($sponsor->members_count / $totalMembersPermissions) * 100, 2),
-            'permissions_counts' => $counts,
-        ];
-    }
-    return null;
-})->filter();
+                $result = [
+                    'id' => $sponsor->id,
+                    'name' => $sponsor->name,
+                    'name_e' => $sponsor->name_e,
+                    'count' => $sponsor->members_count,
+                    'percentage' => round(($sponsor->members_count / $totalMembersPermissions) * 100, 2),
+                    'permissions_counts' => $counts,
+                ];
 
 
+                if ($result['count'] > 0 && count($counts) > 0) {
+                    return $result;
+                }
+
+                return null;
+            })->filter()->values()->all();
 
 
         $response = [
@@ -349,7 +344,6 @@ $data['sponsors_members_permissions'] = $memberCountsPermissions->map(function (
 //     public function sponsorStatics()
 //     {
 //         $data = [];
-
 
 // //المجموعات
 //         $data['groups'] = CmSponsorGroup::count();
